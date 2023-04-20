b32b71139bfe7f9a8ce43a1d5e8aba9953126c77
Tim <tperr916@yahoo.com>
2023-04-19 23:10:35 -0400

    Update rubberduck.py

--- rubberduck.py
+++ rubberduck.py
@@ -8,6 +8,8 @@ from os.path import exists
 import time
 import json
 
+tf.device('/GPU:0')
+
 totalTime = time.time()
 
 print("Loading questions...")
@@ -48,40 +50,9 @@ else:
     paddedSequences = data['paddedSequences']
     length = paddedSequences.shape[1]
 
-
 print("Tokenizing took", time.time() - startTime, "seconds")
 startTime = time.time()
 
-print("Loading tags...")
-# load and preprocess tags
-tagsDataframe = pd.read_csv("Tags.csv")
-tagList = tagsDataframe['Tag'].str.split().tolist() 
-
-if not exists("tokenizedTags.npz"):
-    print("Tokenizing tags...")
-    # tokenizing tags
-    tagTokenizer = tf.keras.preprocessing.text.Tokenizer()
-    tagTokenizer.fit_on_texts([str(tag) for tag in tagList])
-    tagSequences = tagTokenizer.texts_to_sequences([str(tag) for tag in tagList])
-    tagLength = max([len(seq) for seq in tagSequences])
-    paddedTagSequences = tf.keras.preprocessing.sequence.pad_sequences(tagSequences, maxlen=tagLength)
-    np.savez("tokenizedTags.npz", tokenizer=tagTokenizer.to_json(), paddedSequences=paddedTagSequences)
-else:
-    print("Loading tokenized tags...")
-    
-    data = np.load('tokenizedTags.npz', allow_pickle=True)
-
-    # this part was an absolute pain to make work
-    tokenizerData = data['tokenizer']
-    tokenizerDataString = json.dumps(tokenizerData.tolist())
-    tokenizerConfig = json.loads(tokenizerDataString)
-    tagTokenizer = tf.keras.preprocessing.text.tokenizer_from_json(tokenizerConfig)
-
-    paddedTagSequences = data['paddedSequences']
-    tagLength = paddedTagSequences.shape[1]
-
-
-print("Tag loading took ", time.time() - startTime, "seconds")
 print("Total preprocessing took ", time.time() - totalTime, "seconds")
 
 if exists("rubbermodel.h5"):
@@ -94,18 +65,9 @@ else:
     embeddingLayer = Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=128)(inputLayer)
     convLayer = Conv1D(filters=128, kernel_size=3, activation="relu")(embeddingLayer)
     poolingLayer = GlobalMaxPooling1D()(convLayer)
-    reshapeLayer = Reshape((1, 128))(poolingLayer) # needed so that the tag layer and the sequences are same shape
-    flattenLayer = Flatten()(reshapeLayer)
 
-    tagInputLayer = Input(shape=(tagLength,))
-    tagEmbeddingLayer = Embedding(input_dim=len(tagTokenizer.word_index) + 1, output_dim=128)(tagInputLayer)
-    tagFlattenLayer = Flatten()(tagEmbeddingLayer)
-
-    # concatenate question and tag embeddings
-    concatLayer = Concatenate()([poolingLayer, tagFlattenLayer])
-
-    outputLayer = Dense(len(categories), activation="softmax")(concatLayer)
-    model = Model(inputs=[inputLayer, tagInputLayer], outputs=outputLayer)
+    outputLayer = Dense(len(categories), activation="softmax")(poolingLayer)
+    model = Model(inputs=inputLayer, outputs=outputLayer)
     model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
     # convert labels to integers
@@ -113,20 +75,27 @@ else:
     labels = [labelEncoder[label] for label in labels]
     labels = np.array(labels)
 
+    paddedSequences = np.array(paddedSequences)
+
+    x = tf.convert_to_tensor(paddedSequences, dtype=tf.float32)
+
+    y = tf.convert_to_tensor(labels, dtype=tf.float32)
+    
     # train model, increase batch size if you want
-    model.fit(x=np.array([np.array(paddedSequences), np.array(paddedTagSequences)]), y=labels, batch_size=32, epochs=10)
+    print("Fitting model...")
+    model.fit(x=paddedSequences, y=labels, epochs=10, batch_size=32)
     model.save("rubbermodel.h5")
     
 print("Model trained")
 print("Total time took", time.time() - totalTime, "seconds")
 exit()
-def classifyQuestion(query, tokenizer, tagTokenizer, length, categories, model):
+def classifyQuestion(query, tokenizer, length, categories, model):
     # process it
     querySequence = tokenizer.texts_to_sequences([query])
     queryPadded = tf.keras.preprocessing.sequence.pad_sequences(querySequence, maxlen=length)
 
     # classify it
-    categoryIndex = np.argmax(model.predict([queryPadded, np.zeros((1, tagLength))]), axis=-1)[0]
+    categoryIndex = np.argmax(model.predict([queryPadded, np.zeros((1))]), axis=-1)[0]
     category = list(categories)[categoryIndex]
 
     # get best answer
